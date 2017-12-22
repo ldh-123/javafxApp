208260fa7a84e2c019ccc25e8fa4eab510785823
ldh123 <ldh123638@163.com>
2017-12-22 17:19:05 +0800

    cvs查看器

---
+++ bootstrapUi/src/main/java/ldh/common/ui/CvsApplication.java
@@ -0,0 +1,29 @@
+package ldh.common.ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import ldh.fx.StageUtil;
+
+/**
+ * Created by xiongfei.lei on 2017/12/21.
+ */
+public class CvsApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        StageUtil.STAGE = primaryStage;
+        Parent node = FXMLLoader.load(JavafxHomeApplication.class.getResource("/fxml/Cvs.fxml"));
+        Scene scene = new Scene(node, 1200, 700);
+        scene.getStylesheets().add(this.getClass().getResource("/css/Cvs.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("CVS查看器");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
